0c141efff2914aa22bbf0cb9dd8fb83626c4bd17
Matt English <menglish2451@gmail.com>
2017-07-20 07:15:38 -0700

    Add cookie-session

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const express = require( 'express' );
 const helmet = require('helmet');
 const hbs = require( 'express-handlebars' );
+const cookieSession = require('cookie-session');
 
 const PORT = 8080;
 const HOST = '0.0.0.0';
@@ -10,6 +11,17 @@ const HOST = '0.0.0.0';
 const app = express();
 // Set HTTP headers
 app.use(helmet());
+
+
+// set cookie session
+app.set('trust proxy', 1) // trust first proxy
+app.use(cookieSession({
+  name: 'session',
+  keys: ['key1', 'key2'],
+ 
+  // Cookie Options 
+  maxAge: 24 * 60 * 60 * 1000 // 24 hours 
+}))
 
 // Set the /views/layouts/main.handlebars as the base template
 app.engine( 'handlebars', hbs( {
@@ -22,6 +34,12 @@ app.use( '/public', express.static( __dirname + '/build' ) );
 
 // Fetch the homepage
 app.get( '/', function ( req, res ) {
+    // Update views 
+    req.session.views = (req.session.views || 0) + 1
+    // Write response 
+    // TODO - move this to a data point 
+    // res.end(req.session.views + ' views')
+
     res.render( 'home' );
 } );
 
