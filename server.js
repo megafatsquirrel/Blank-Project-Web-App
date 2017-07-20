c77a4734442042a36c7fb56b0f7a07c6b2e00a06
Matt English <menglish2451@gmail.com>
2017-07-19 17:22:38 -0700

    Add helmet for setting http headers

--- server.js
+++ server.js
@@ -1,23 +1,37 @@
-var express = require( 'express' );
-var app = express();
+'use strict';
 
-var hbs = require( 'express-handlebars' );
+const express = require( 'express' );
+const helmet = require('helmet');
+const hbs = require( 'express-handlebars' );
+
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+const app = express();
+// Set HTTP headers
+app.use(helmet());
+
+// Set the /views/layouts/main.handlebars as the base template
 app.engine( 'handlebars', hbs( {
     defaultLayout: 'main'
 } ) );
+
+// Set the framework to be used for the front-end
 app.set( 'view engine', 'handlebars' );
 app.use( '/public', express.static( __dirname + '/build' ) );
 
-// Homepage
+// Fetch the homepage
 app.get( '/', function ( req, res ) {
     res.render( 'home' );
 } );
 
+// Example endpoint for getting raw data
 app.get( '/returnJSON', function ( req, res ) {
     res.json( { item1 : 'one', item2 : 'item2' } );
 } );
 
-var port = Number( process.env.PORT || 3000 );
-var server = app.listen( port, function () {
+const port = Number( process.env.PORT || PORT );
+const server = app.listen( port, function () {
     console.log( 'Started Server: Port %d', server.address().port );
+    console.log(`Running on http://${HOST}:${PORT}`);
 } );
